9f127bd109af8ba79df9f0c6e9b68cd0312222ac
Mina Yoo <maydec2298@gmail.com>
2022-08-19 21:33:44 +0900

    redux_counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { addNumber, minusNumber } from "./redux/modules/counter"
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+  const globalNumber = useSelector((state) => state.counter.number);
+
+  const onChangeHandler = (event) => {
+    const { value } = event.target;
+    setNumber(+value);
+    // event.target.value는 문자열 입니다.
+    // 이것을 숫자형으로 형변환해주기 위해서 +를 붙여 주었습니다.
+  };
+
+  // 콘솔로 onChangeHandler가 잘 연결되었는지 확인해봅니다.
+  // input에 값을 넣을 때마다 콘솔에 그 값이 찍히면 연결 성공!
+  // console.log(number);
+
+  // 2. 더하기 버튼을 눌렀을 때 실행할 이벤트핸들러를 만들어줍니다.
+  const onClickAddNumberHandler = () => {
+    // 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
+    dispatch(addNumber(number));
+  };
+
+  const onClickMinusNumberHandler = () => {
+    dispatch(minusNumber(number));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{globalNumber}</div>
+      <input type="number" onChange={onChangeHandler} />
+      <button onClick={onClickAddNumberHandler} >더하기</button>
+      <button onClick={onClickMinusNumberHandler}>빼기</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/redux/modules/counter.js
@@ -0,0 +1,49 @@
+// src/redux/modules/counter.js
+
+// Action Value
+const ADD_NUMBER = "ADD_NUMBER";
+const MINUS_NUMBER = "MINUS_NUMBER";
+
+// Action Creator
+export const addNumber = (payload) => {
+  return {
+    type: ADD_NUMBER,
+    // payload: payload,
+    payload,
+  };
+};
+
+export const minusNumber = (payload) => {
+  return {
+    type: MINUS_NUMBER,
+    payload,
+  }
+}
+
+// Initial State
+const initialState = {
+  number: 0,
+};
+
+// Reducer
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NUMBER:
+      return {
+        number: state.number + action.payload,
+      };
+
+    case MINUS_NUMBER:
+      return {
+        number: state.number - action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+
+
+// export default reducer
+export default counter;
